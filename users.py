d2d78e731f8e5b9fca3e722b0b34a0b8ec754a1c
Anna Essina <anna.essina@helsinki.fi>
2023-02-18 17:36:32 +0200

    users.py changes

--- users.py
+++ users.py
@@ -1,1 +1,36 @@
+import os
+from db import db
+from flask import session
+from sqlalchemy.sql import text
+from werkzeug.security import check_password_hash, generate_password_hash
 
+def login(username, password):
+    sql = text("SELECT id, password FROM users WHERE username=:username")
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        hash_value = user.password
+        if not check_password_hash(hash_value, password):
+            return False
+        session["user_id"] = user[1]
+        session["user_name"] = username
+        session["csrf_token"] = os.urandom(16).hex()
+        return True
+
+def register(username,password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = text("""INSERT INTO users (username, password) VALUES (:username, :password)""")
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    except:
+        return False
+
+    return login(username, password)
+
+
+def logout():
+    del session["user_id"]
+    del session["user_name"]
